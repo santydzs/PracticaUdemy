f979c3978fd7d7e136e757c1964619e488a176f7
Alessio <santiago.alessio@capgemini.com>
2019-02-01 11:44:33 -0300

    se agrega componente calculadora, aun no terminado

--- src/app/Components/calculadora/calculadora.component.ts
+++ src/app/Components/calculadora/calculadora.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor() { }
+  operacion: string;
+
+  constructor() {
+    this.operacion = "+";
+   }
 
   ngOnInit() {
   }
 
+  CambiarOp(signo: string): void{
+    this.operacion = signo;
+  }  
 }
