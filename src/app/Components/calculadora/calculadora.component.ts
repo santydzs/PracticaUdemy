cb5ff3ce372fa5ef673a3d0b117e427a5829f62b
Alessio <santiago.alessio@capgemini.com>
2019-02-02 14:59:09 -0300

    Calculadora finalizada, parte 1 del curso finalizada

--- src/app/Components/calculadora/calculadora.component.ts
+++ src/app/Components/calculadora/calculadora.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { resource } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-calculadora',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class CalculadoraComponent implements OnInit {
 
   operacion: string;
+  campo1: number;
+  campo2: number;
+  resultado: number;
+  visible: boolean;
+  historial: string[];
 
   constructor() {
     this.operacion = "+";
+    this.campo1 = 0;
+    this.campo2 = 0;
+    this.resultado = 0;
+    this.visible = false;
+    this.historial = [];
    }
 
   ngOnInit() {
@@ -18,5 +29,33 @@ export class CalculadoraComponent implements OnInit {
 
   CambiarOp(signo: string): void{
     this.operacion = signo;
-  }  
+  }
+  
+  CargarHistorial(){
+    var operacion = this.campo1.toString() + ' ' + this.operacion + ' ' + this.campo2.toString() + ' = ' + this.resultado.toString();
+    this.historial.push(operacion);
+  }
+
+  Ejecutar(): void{
+    switch(this.operacion){
+      case "+":{
+        this.resultado = this.campo1 + this.campo2;
+        break;
+      }
+      case "-":{
+        this.resultado = this.campo1 - this.campo2;
+        break;
+      }
+      case "/":{
+        this.resultado = this.campo1 / this.campo2;
+        break;
+      }
+      case "x":{
+        this.resultado = this.campo1 * this.campo2;
+        break;
+      }
+    }
+
+    this.CargarHistorial();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './Components/header/header.component';
-import { CalculadoraComponent } from './Components/calculadora/calculadora.component'
+import { CalculadoraComponent } from './Components/calculadora/calculadora.component';
+import { FooterComponent } from './Components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
